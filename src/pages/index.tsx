19f81c81b263b146e621f130bc0c9e23d2e9b90a
Zakaria Elharmouzi <z.elharmouzi@aui.ma>
2023-07-02 17:31:31 +0100

    Made a sekeleton for the website needs upd info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,240 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Typewriter from "typewriter-effect";
+import Arrow from "../../public/assets/arrow.png";
+import Image from "next/image";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import Pfp from "../../public/assets/pfpp.png";
+import SchoolIcon from "../../public/assets/icon-school.svg";
+import Python from "../../public/assets/icons8-python.svg";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-black">
+      {/* <Navbar /> */}
+      <div className="flex p-10 pt-20 pb-20 lg:p-24 flex-col lg:flex-row text-white lg:gap-10">
+        <div className="lg:w-1/2 lg:p-12 lg:pr-6 lg:pb-32">
+          <h1 className="lg:text-5xl text-5xl font-JetBrainsMono font-semibold mb-10 lg:mb-10">
+            {">"} Zakaria <span>Elharmouzi_</span>
+          </h1>
+          <h1 className="lg:text-5xl text-5xl font-Montserrat font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 mb-5 lg:mb-10">
+            {/* <h1 className="lg:text-5xl font-Montserrat font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 background-animate"> */}
+            Fullstack developer <span className="text-white">&</span> Software
+            Engineer
+          </h1>
+          <p className="text-justify mb-10 text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <AnchorLink href="#about">
+            <h1 className="px-6 py-2 text-black font-JetBrainsMono font-bold text-xl bg-white rounded-md w-full flex items-center justify-center lg:w-fit brightness-100 hover:brightness-75 transition-all duration-150 mb-10 lg:mb-0">
+              About me
+            </h1>
+          </AnchorLink>
+        </div>
+        <div className="lg:w-1/2 h-80 lg:h-auto lg:p-12 lg:pb-24">
+          <div className="bg-pfp h-full bg-cover bg-center border-[1px] border-white box-shadoww"></div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="lg:px-44 lg:py-24 p-10 pt-14 pb-14 bg-white" id="about">
+        <div className="flex flex-col items-center justify-center text-black">
+          <h1 className="font-JetBrainsMono text-5xl font-bold mb-10 ">
+            About_
+          </h1>
+          <p className="text-lg text-justify mb-16">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+          </p>
+          <div className="flex flex-col lg:flex-row gap-20 w-full">
+            <div className="lg:w-1/3 flex flex-col items-center justify-center">
+              <Image
+                className="w-12 rounded-[20%] p-2 border-black border-2 mb-4"
+                src={SchoolIcon}
+                alt="school icon"
+              />
+              <h1 className="text-black font-semibold text-2xl">
+                Junior at AUI
+              </h1>
+            </div>
+            <div className="lg:w-1/3 flex flex-col items-center justify-center">
+              <Image
+                className="w-12 rounded-[20%] p-2 border-black border-2 mb-4"
+                src={SchoolIcon}
+                alt="school icon"
+              />
+              <h1 className="text-black font-semibold text-2xl">
+                Junior at AUI
+              </h1>
+            </div>
+            <div className="lg:w-1/3 flex flex-col items-center justify-center">
+              <Image
+                className="w-12 rounded-[20%] p-2 border-black border-2 mb-4"
+                src={SchoolIcon}
+                alt="school icon"
+              />
+              <h1 className="text-black font-semibold text-2xl">
+                Junior at AUI
+              </h1>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="lg:px-44 lg:py-24 p-10 pt-14 pb-14" id="skills">
+        <div className="flex flex-col lg:flex-row gap-10 lg:gap-20">
+          <div className="lg:w-1/2">
+            <h1 className="font-JetBrainsMono text-5xl bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500 font-bold mb-8">
+              Skills & Experience_
+            </h1>
+            <p className="text-white mb-10">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <h1 className="font-JetBrainsMono text-3xl text-white font-bold mb-14 lg:mb-8">
+              Skills
+            </h1>
+            <div className="grid grid-cols-1 gap-10 lg:grid-cols-3 w-full">
+              <div className="flex flex-row items-center lg:justify-center mb-5">
+                <Image className="w-12 mr-4" src={Python} alt="school icon" />
+                <h1 className="text-white font-semibold text-2xl">Python</h1>
+              </div>
+              <div className="flex flex-row items-center lg:justify-center mb-5">
+                <Image className="w-12 mr-4" src={Python} alt="school icon" />
+                <h1 className="text-white font-semibold text-2xl">Python</h1>
+              </div>
+              <div className="flex flex-row items-center lg:justify-center mb-5">
+                <Image className="w-12 mr-4" src={Python} alt="school icon" />
+                <h1 className="text-white font-semibold text-2xl">Python</h1>
+              </div>
+              <div className="flex flex-row items-center lg:justify-center mb-5">
+                <Image className="w-12 mr-4" src={Python} alt="school icon" />
+                <h1 className="text-white font-semibold text-2xl">Python</h1>
+              </div>
+              <div className="flex flex-row items-center lg:justify-center mb-5">
+                <Image className="w-12 mr-4" src={Python} alt="school icon" />
+                <h1 className="text-white font-semibold text-2xl">Python</h1>
+              </div>
+              <div className="flex flex-row items-center lg:justify-center mb-5">
+                <Image className="w-12 mr-4" src={Python} alt="school icon" />
+                <h1 className="text-white font-semibold text-2xl">Python</h1>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/2 lg:px-4 text-white">
+            <h1 className="font-JetBrainsMono text-3xl text-white font-bold mb-10">
+              Experience
+            </h1>
+            <div className="flex flex-col">
+              <div className="border-t-2 flex flex-row border-white pt-4 mb-12">
+                <div className="w-1/3">
+                  <h1>2020 - Present</h1>
+                </div>
+                <div className="w-2/3 flex flex-col">
+                  <h1 className="mb-6">Product Designer</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor
+                  </p>
+                </div>
+              </div>
+              <div className="border-t-2 flex flex-row border-white pt-4 mb-12">
+                <div className="w-1/3">
+                  <h1>2020 - Present</h1>
+                </div>
+                <div className="w-2/3 flex flex-col">
+                  <h1 className="mb-6">Product Designer</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor
+                  </p>
+                </div>
+              </div>
+              <div className="border-t-2 flex flex-row border-white pt-4 ">
+                <div className="w-1/3">
+                  <h1>2020 - Present</h1>
+                </div>
+                <div className="w-2/3 flex flex-col">
+                  <h1 className="mb-6">Product Designer</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
-  )
+      <div className="bg-white" id="project">
+        <div className="lg:px-44 lg:py-24  p-10 pt-14 pb-14">
+          <h1 className="font-JetBrainsMono text-5xl font-bold mb-10 ">
+            Projects_
+          </h1>
+          <p className="text-lg mb-8 text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+          </p>
+        </div>
+        <div className="flex flex-col lg:flex-row lg:h-80">
+          <div className="lg:w-[25%] h-80  first-image bg-center bg-cover translate-all duration-200 hover:w-[33%] cursor-pointer p-10 flex flex-col">
+            <h1 className="text-white font-bold font-JetBrainsMono text-2xl mb-6">
+              M6L website
+            </h1>
+            <p className="text-justify font-semibold text-white ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+          <div className="lg:w-[25%] h-80  second-image bg-center bg-cover translate-all duration-200 hover:w-[33%] cursor-pointer p-10 flex flex-col">
+            <h1 className="text-white font-bold font-JetBrainsMono text-2xl mb-6">
+              M6L website
+            </h1>
+            <p className="text-justify font-semibold text-white ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+          <div className="lg:w-[25%] h-80  third-image bg-center bg-cover translate-all duration-200 hover:w-[33%] cursor-pointer p-10 flex flex-col">
+            <h1 className="text-white font-bold font-JetBrainsMono text-2xl mb-6">
+              M6L website
+            </h1>
+            <p className="text-justify font-semibold text-white ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+          <div className="lg:w-[25%] h-80  fourth-image bg-center bg-cover translate-all duration-200 hover:w-[33%] cursor-pointer p-10 flex flex-col">
+            <h1 className="text-white font-bold font-JetBrainsMono text-2xl mb-6">
+              M6L website
+            </h1>
+            <p className="text-justify font-semibold text-white ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        pfp: "url('../../public/assets/pfp.jpg')",
+        lib: "url('../../public/assets/lib.jpg')",
+        data: "url('../../public/assets/data.jpg')",
+        electric: "url('../../public/assets/electric.jpg')",
+        fuel: "url('../../public/assets/fuel.jpg')",
+      },
+      fontFamily: {
+        JetBrainsMono: ["JetBrains Mono", "monospace"],
+        Poppins: ["Poppins", "sans-serif"],
+        Raleway: ["Raleway", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+        Merriweather: ["Merriweather", "sans-serif"],
+        Lato: ["Lato"],
+        Montserrat: ["Montserrat"],
+        Bebas: ["Bebas Neue"],
+        Sacramento: ["Sacramento"],
+        Belle: ["La Belle Aurore"],
       },
     },
   },
   plugins: [],
-}
+};
